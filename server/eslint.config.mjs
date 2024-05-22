082ae35233bd6f2889202eea8139b46748a17b3d
TSantiagoIdalgo <tomasidalgo51@gmail.com>
2024-05-22 18:06:09 -0300

    first commit

---
+++ server/eslint.config.mjs
@@ -0,0 +1,20 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+
+export default [
+  {languageOptions: { globals: globals.node }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.stylistic,
+  ...tseslint.configs.strict,
+  {
+    'rules': {
+      'indent': ['error', 2],
+      'no-console': 'off',
+      'one-var': 'off',
+      'quotes': ['error', 'single'],
+      'semi': 'error',
+    }
+  }
+];
