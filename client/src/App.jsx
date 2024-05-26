c15ab4d5d640e477523b0dfd645bf26e9997929b
Diego Cuevas <ingdcuevas@gmail.com>
2024-05-26 09:52:37 -0500

    restableciendo componente App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Welcome to Chameguiño</h1>
+      <h1 className="">Welcome to Chameguiño</h1>
       <h2>{count}</h2>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>decrement</button>
